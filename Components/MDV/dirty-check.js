e2404583718bcab8e26bb55863c72cfb18031f53
Scott Miles <sjmiles@google.com>
2012-12-18 12:48:11 -0800

    add customized MDV polyfill to Components polyfill suite

---
+++ Components/MDV/dirty-check.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2012 The Toolkitchen Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+(function() {
+  check = function() {
+    Model.dirtyCheck();
+  };
+  window.addEventListener('WebComponentsReady', function() {
+    check();
+    setInterval(check, 250);
+  });
+})();
--- Components/components-polyfill.js
+++ Components/components-polyfill.js
@@ -77,6 +77,7 @@ var require = function(inSrc) {
   "ComponentDocuments/parser.js",
   "CustomDOMElement/CustomDOMElements.js",
   "CustomDOMElement/HTMLElementElement.js",
+  "MDV/mdv-polyfill.js",
   "lib/boot.js"
 ].forEach(require);
 
